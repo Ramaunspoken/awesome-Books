ea292ddaf17f16be2a2d8cb03aca72ec751b7c33
AMOS <108424978+Ramaunspoken@users.noreply.github.com>
2022-12-07 18:04:04 +0300

    Update index.js

--- index.js
+++ index.js
@@ -59,7 +59,7 @@ ManageBooks.prototype.display = function () {
         this.delete(e);
       });
    } else {
-    bookList.innerText = 'Empty space';
+    bookList.innerText = 'Empty Book list,';
   }
 };
   
